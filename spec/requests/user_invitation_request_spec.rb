f924099bf2f8973011f0906417cadeaf881cc28b
MayankSaraf <mayanksar87@gmail.com>
2023-06-19 18:47:01 +0530

    fixed rspec

--- spec/requests/user_invitation_request_spec.rb
+++ spec/requests/user_invitation_request_spec.rb
@@ -19,25 +19,4 @@ RSpec.describe UserInvitationsController, type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
-
-  describe 'sign in' do
-    it 'returns a successful response' do
-      post '/auth/sign_in',params: {
-        "email": 'john@gmail.com',
-        "password": "password"
-      } 
-      @response_uid = response.header["uid"]
-      @response_access_token = response.header["access-token"]
-      @response_client = response.header["client"]
-      @response_authorization = response.header["Authorization"]
-      expect(response).to have_http_status(:ok)
-    end
-  end
-  
-  describe 'index request' do 
-    it 'get request' do
-      get '/user_invitations', params: {}, headers: {'access-token' => @response_access_token, 'uid' => @response_uid, 'client' => @response_client, 'Authorization' => @response_authorization}
-      expect(response).to have_http_status(:ok)
-    end
-  end
 end
