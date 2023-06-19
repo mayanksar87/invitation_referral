7deaffaf526937c11e4270c9d4ae6a3c036fb4c8
MayankSaraf <mayanksar87@gmail.com>
2023-06-19 13:45:03 +0530

    Error component

--- app/javascript/components/application.tsx
+++ app/javascript/components/application.tsx
@@ -1,15 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import App from "./App";
 
-interface AppProps {
-  arg: string;
-}
-
-const App = ({ arg }: AppProps) => {
-  return <div>{`Hello, ${arg}!`}</div>;
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  const rootEl = document.getElementById("root");
-  ReactDOM.render(<App arg="Rails 7 with ESBuild" />, rootEl);
-});
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
